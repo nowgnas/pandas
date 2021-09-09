71697d41f1202fd88742f5faf814336798214e71
s-wlii <marshmello.sw@gmail.com>
2021-09-09 12:45:47 +0900

    add files

--- chapter7/sec_02_row_name.py
+++ chapter7/sec_02_row_name.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from importFile import *
 
 ebola = pd.read_csv('../doit_pandas/data/country_timeseries.csv')
@@ -16,5 +18,18 @@ country_values = variable_split.str.get(1)
 
 ebola_long['status'] = status_values
 ebola_long['country'] = country_values
-print(ebola_long.head())
+# print(ebola_long.head())
 # p.163
+
+weather = pd.read_csv('../doit_pandas/data/weather.csv')
+# print(weather.iloc[:5, :])
+weather_melt = pd.melt(weather, id_vars=['id', 'year', 'month', 'element'],
+                       var_name='day', value_name='temp')
+# print(weather_melt.head())
+
+weather_tidy = weather_melt.pivot_table(
+    index=['id', 'year', 'month', 'day'],
+    columns='element',
+    values='temp'
+)
+print(weather_tidy)
